850677b2f8c7c61009d61ad470a5f53ff47bc02d
“AndreaVerdolini” <andreaverdolini6@gmail.com>
2023-06-09 11:14:45 +0200

    modificate le relazioni

--- progetto-siw/src/main/java/it/uniroma3/siw/model/OrderLine.java
+++ progetto-siw/src/main/java/it/uniroma3/siw/model/OrderLine.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 
 @Entity
@@ -13,6 +14,10 @@ public class OrderLine {
 	private Long id;
 	private int costoUnitario;
 	private int quantita;
+
+	@ManyToOne
+	private Product product;
+
 	public Long getId() {
 		return id;
 	}
@@ -30,6 +35,35 @@ public class OrderLine {
 	}
 	public void setQuantita(int quantita) {
 		this.quantita = quantita;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderLine other = (OrderLine) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	
--- progetto-siw/src/main/java/it/uniroma3/siw/model/PriceList.java
+++ progetto-siw/src/main/java/it/uniroma3/siw/model/PriceList.java
@@ -1,9 +1,13 @@
 package it.uniroma3.siw.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 @Entity
@@ -12,6 +16,14 @@ public class PriceList {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
+
+	@OneToMany
+	private List<PriceListItem> priceListItems;
+
+	public PriceList() {
+		this.priceListItems = new ArrayList<PriceListItem>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -23,6 +35,37 @@ public class PriceList {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public List<PriceListItem> getPriceListItems() {
+		return priceListItems;
+	}
+	public void setPriceListItems(List<PriceListItem> priceListItems) {
+		this.priceListItems = priceListItems;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PriceList other = (PriceList) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	
