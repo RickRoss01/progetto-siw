a510e9bdcb53aae636a12d512afcc533aaa064d5
Andrea <andreaverdolini6@gmail.com>
2023-06-14 12:33:17 +0200

    modifiche a customers e aggiunta di customer

--- progetto-siw/src/main/java/it/uniroma3/siw/controller/CustomerController.java
+++ progetto-siw/src/main/java/it/uniroma3/siw/controller/CustomerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.siw.service.CustomerService;
 
@@ -18,5 +19,11 @@ public class CustomerController{
     private String getCustomers(Model model){
         model.addAttribute("customers",this.customerService.getAllCustomersBy50(1));
         return "customers.html";
+    }
+
+    @GetMapping(value = "/customer/{customerId}")
+    private String getCustomer(@PathVariable("customerId") Long customerId, Model model){
+        model.addAttribute("customer",this.customerService.getCusomer(customerId));
+        return "customer.html";
     }
 }
