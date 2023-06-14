6c3d405cfffff54419069fd8da7388438bbecc88
Riccardo Sebastianelli <riccardinoseba01@gmail.com>
2023-06-14 23:32:53 +0200

    Gestita validazione e form customer, da finire

--- progetto-siw/src/main/java/it/uniroma3/siw/repository/CustomerRepository.java
+++ progetto-siw/src/main/java/it/uniroma3/siw/repository/CustomerRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Customer;
 
@@ -12,5 +13,14 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
     @Query("SELECT c FROM Customer c ORDER BY createdOn ASC")
     List<Customer> findAllCustomers(Pageable pageable);
 
-    public boolean existsByRagioneSocialeAndIndirizzo(String ragioneSociale, String indirizzo);
+    public boolean existsByragioneSocialeAndIndirizzo(String ragioneSociale, String indirizzo);
+
+    public boolean existsBypIva(String pIva);
+
+    public int countBypIva(String getpIva);
+
+    @Query(value = "SELECT COUNT(*) FROM customer " + 
+            "WHERE p_iva = :pIva " + 
+            "AND id <> :customerId", nativeQuery=true)
+    int countOtherCustomersWithSamePIva(@Param("customerId") Long id,@Param("pIva") String getpIva);
 }
