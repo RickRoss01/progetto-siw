06a079dbf37b3fc98ad7fdab5ef0f182defe0440
AndreaCiamp <aciamponi@gmail.com>
2023-06-19 14:41:59 +0200

    Gestione Contact

--- progetto-siw/src/main/java/it/uniroma3/siw/model/Contact.java
+++ progetto-siw/src/main/java/it/uniroma3/siw/model/Contact.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Entity
@@ -13,10 +16,18 @@ public class Contact {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
+	@NotNull
 	private String cognome;
-	private int telefono;
+	private String nazione;
+	private String indirizzo;
+	private String cap;
+	private Integer telefono;
+	@NotNull
+	private String email;
+	private Integer numeroCivico;
 
 	@ManyToOne
+	@JoinColumn(name = "customer_id", updatable = false)
 	private Customer customer;
 
 	public Long getId() {
@@ -37,11 +48,82 @@ public class Contact {
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
-	public int getTelefono() {
+	public String getNazione() {
+		return nazione;
+	}
+	public void setNazione(String nazione) {
+		this.nazione = nazione;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getCap() {
+		return cap;
+	}
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+	public Integer getTelefono() {
 		return telefono;
 	}
-	public void setTelefono(int telefono) {
+	public void setTelefono(Integer telefono) {
 		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getNumeroCivico() {
+		return numeroCivico;
+	}
+	public void setNumeroCivico(Integer numeroCivico) {
+		this.numeroCivico = numeroCivico;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (cognome == null) {
+			if (other.cognome != null)
+				return false;
+		} else if (!cognome.equals(other.cognome))
+			return false;
+		return true;
 	}
 	
 }
